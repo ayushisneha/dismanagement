ce582123daee7de5e448caaaba2846b44fad94f5
Ayushi Sneha <30649556+ayushisneha@users.noreply.github.com>
2018-10-26 10:09:20 +0530

    Add files via upload

--- home/models.py
+++ home/models.py
@@ -3,11 +3,19 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class City(models.Model):
+    cityname = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, default=None, blank=True)
+    distype = models.CharField(max_length=50, default=None, blank=True)
+
+    def __str__(self):
+        return self.cityname
+
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     portfolio_site = models.URLField(blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    cityname = models.ForeignKey(City, on_delete=models.CASCADE)
 
-
-def __str__(self):
-    return self.user.username
+    def __str__(self):
+        return self.user.username
